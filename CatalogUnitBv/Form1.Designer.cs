7ea3edce5bae9302951801b8981d854b42e3d92d
Daniel Cocu <daniel.cocu4@gmail.com>
2024-05-09 16:02:50 +0300

    update

--- CatalogUnitBv/Form1.Designer.cs
+++ CatalogUnitBv/Form1.Designer.cs
@@ -30,17 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
-            Parola = new Label();
+            lblPass = new Label();
             Autentificare = new Button();
             Email = new TextBox();
-            textBox2 = new TextBox();
+            Parola = new TextBox();
+            Save = new CheckBox();
+            Recovery = new Label();
             label5 = new Label();
             label4 = new Label();
             label6 = new Label();
-            panel1 = new Panel();
-            Save = new CheckBox();
-            Recovery = new Label();
-            panel1.SuspendLayout();
+            ImgFundal = new Panel();
+            panel2 = new Panel();
+            ImgFundal.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -48,100 +49,66 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.ForeColor = Color.White;
-            label1.Location = new Point(101, 221);
+            label1.Location = new Point(127, 169);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(54, 25);
+            label1.Size = new Size(36, 15);
             label1.TabIndex = 1;
             label1.Text = "Email";
             // 
-            // Parola
+            // lblPass
             // 
-            Parola.AutoSize = true;
-            Parola.BackColor = Color.Transparent;
-            Parola.ForeColor = Color.White;
-            Parola.Location = new Point(101, 292);
-            Parola.Name = "Parola";
-            Parola.Size = new Size(60, 25);
-            Parola.TabIndex = 2;
-            Parola.Text = "Parola";
+            lblPass.AutoSize = true;
+            lblPass.BackColor = Color.Transparent;
+            lblPass.ForeColor = Color.White;
+            lblPass.Location = new Point(127, 201);
+            lblPass.Margin = new Padding(2, 0, 2, 0);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(40, 15);
+            lblPass.TabIndex = 2;
+            lblPass.Text = "Parola";
             // 
             // Autentificare
             // 
             Autentificare.BackColor = Color.FromArgb(238, 149, 76);
-            Autentificare.Location = new Point(233, 401);
+            Autentificare.Location = new Point(209, 288);
+            Autentificare.Margin = new Padding(2);
             Autentificare.Name = "Autentificare";
-            Autentificare.Size = new Size(141, 44);
+            Autentificare.Size = new Size(99, 26);
             Autentificare.TabIndex = 3;
             Autentificare.Text = "Autentificare";
             Autentificare.UseVisualStyleBackColor = false;
             // 
             // Email
             // 
-            Email.Location = new Point(101, 249);
+            Email.BackColor = Color.FromArgb(28, 27, 31);
+            Email.ForeColor = Color.White;
+            Email.Location = new Point(123, 180);
+            Email.Margin = new Padding(2);
             Email.Name = "Email";
-            Email.Size = new Size(403, 31);
+            Email.Size = new Size(283, 23);
             Email.TabIndex = 4;
             // 
-            // textBox2
+            // Parola
             // 
-            textBox2.Location = new Point(101, 320);
-            textBox2.Name = "textBox2";
-            textBox2.PasswordChar = '*';
-            textBox2.Size = new Size(403, 31);
-            textBox2.TabIndex = 5;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.Transparent;
-            label5.ForeColor = Color.White;
-            label5.Location = new Point(89, 7);
-            label5.Name = "label5";
-            label5.Size = new Size(113, 25);
-            label5.TabIndex = 8;
-            label5.Text = "Universitatea";
-            label5.Click += label5_Click;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.Transparent;
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(89, 32);
-            label4.Name = "label4";
-            label4.Size = new Size(101, 25);
-            label4.TabIndex = 7;
-            label4.Text = "Transilvania";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.BackColor = Color.Transparent;
-            label6.ForeColor = Color.White;
-            label6.Location = new Point(89, 57);
-            label6.Name = "label6";
-            label6.Size = new Size(95, 25);
-            label6.TabIndex = 9;
-            label6.Text = "din Brasov";
-            // 
-            // panel1
-            // 
-            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
-            panel1.Controls.Add(label5);
-            panel1.Controls.Add(label6);
-            panel1.Controls.Add(label4);
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(598, 206);
-            panel1.TabIndex = 10;
+            Parola.BackColor = Color.FromArgb(28, 27, 31);
+            Parola.ForeColor = Color.White;
+            Parola.Location = new Point(123, 211);
+            Parola.Margin = new Padding(2);
+            Parola.Name = "Parola";
+            Parola.PasswordChar = '*';
+            Parola.Size = new Size(283, 23);
+            Parola.TabIndex = 5;
             // 
             // Save
             // 
+            Save.AllowDrop = true;
             Save.AutoSize = true;
             Save.ForeColor = Color.White;
-            Save.Location = new Point(101, 366);
+            Save.Location = new Point(123, 242);
+            Save.Margin = new Padding(2);
             Save.Name = "Save";
-            Save.Size = new Size(152, 29);
+            Save.Size = new Size(104, 19);
             Save.TabIndex = 11;
             Save.Text = "Tine-ma minte";
             Save.UseVisualStyleBackColor = true;
@@ -150,45 +117,111 @@
             // 
             Recovery.AutoSize = true;
             Recovery.ForeColor = Color.FromArgb(238, 149, 76);
-            Recovery.Location = new Point(309, 366);
+            Recovery.Location = new Point(268, 244);
+            Recovery.Margin = new Padding(2, 0, 2, 0);
             Recovery.Name = "Recovery";
-            Recovery.Size = new Size(195, 25);
+            Recovery.Size = new Size(130, 15);
             Recovery.TabIndex = 12;
             Recovery.Text = "Probleme la conectare?";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(117, 80);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 21);
+            label5.TabIndex = 8;
+            label5.Text = "Universitatea";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(117, 101);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Transilvania";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(117, 122);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(83, 21);
+            label6.TabIndex = 9;
+            label6.Text = "din Brasov";
+            // 
+            // ImgFundal
+            // 
+            ImgFundal.BackgroundImage = (Image)resources.GetObject("ImgFundal.BackgroundImage");
+            ImgFundal.Controls.Add(label6);
+            ImgFundal.Controls.Add(label4);
+            ImgFundal.Controls.Add(panel2);
+            ImgFundal.Controls.Add(label5);
+            ImgFundal.Location = new Point(1, 2);
+            ImgFundal.Name = "ImgFundal";
+            ImgFundal.Size = new Size(548, 155);
+            ImgFundal.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.Location = new Point(39, 71);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(73, 72);
+            panel2.TabIndex = 0;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(28, 27, 31);
-            ClientSize = new Size(598, 465);
+            ClientSize = new Size(545, 351);
+            Controls.Add(ImgFundal);
+            Controls.Add(lblPass);
+            Controls.Add(label1);
             Controls.Add(Recovery);
             Controls.Add(Save);
-            Controls.Add(panel1);
-            Controls.Add(textBox2);
+            Controls.Add(Parola);
             Controls.Add(Email);
             Controls.Add(Autentificare);
-            Controls.Add(Parola);
-            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
             Name = "Form1";
+            ShowInTaskbar = false;
             Text = "Catalog UnitBv";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            ImgFundal.ResumeLayout(false);
+            ImgFundal.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
         private Label label1;
-        private Label Parola;
+        private Label lblPass;
         private Button Autentificare;
         private TextBox Email;
-        private TextBox textBox2;
+        private TextBox Parola;
+        private CheckBox Save;
+        private Label Recovery;
         private Label label5;
         private Label label4;
         private Label label6;
-        private Panel panel1;
-        private CheckBox Save;
-        private Label Recovery;
+        private Panel ImgFundal;
+        private Panel panel2;
     }
 }
--- CatalogUnitBv/Form1.cs
+++ CatalogUnitBv/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace CatalogUnitBv
 {
@@ -15,9 +18,42 @@ namespace CatalogUnitBv
         public Form1()
         {
             InitializeComponent();
+            Autentificare.FlatAppearance.BorderSize = 0;
+            Autentificare.FlatStyle = FlatStyle.Flat;
+
+            int radius = 15; // Mărimea razei pentru colțurile curbate
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius * 2, radius * 2, 180, 90);
+            path.AddArc(Autentificare.Width - radius * 2, 0, radius * 2, radius * 2, 270, 90);
+            path.AddArc(Autentificare.Width - radius * 2, Autentificare.Height - radius * 2, radius * 2, radius * 2, 0, 90);
+            path.AddArc(0, Autentificare.Height - radius * 2, radius * 2, radius * 2, 90, 90);
+            path.CloseAllFigures();
+            Autentificare.Region = new Region(path);
+
+            
+
+
+            Save.FlatAppearance.BorderColor = Color.White;// Culoare transparentă pentru bordură
+            Save.FlatAppearance.CheckedBackColor = Color.FromArgb(238, 149, 76);
+            Email.BorderStyle = BorderStyle.None; // Elimină stilul implicit al bordurii
+            Email.BackColor = Color.FromArgb(37, 36, 40); // Setează culoarea fundalului
+            Email.ForeColor = Color.White; // Setează culoarea textului
+          
+           
+
+            Parola.BorderStyle = BorderStyle.None;
+            Parola.BackColor = Color.FromArgb(37, 36, 40); // Setează culoarea fundalului
+            Parola.ForeColor = Color.White; // Setează culoarea textului
         }
 
+       
+
         private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Autentificare_Click(object sender, EventArgs e)
         {
 
         }
