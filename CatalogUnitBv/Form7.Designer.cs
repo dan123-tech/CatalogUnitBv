c28fc2f01131048c3541b863ada94c842bea7f92
Daniel Cocu <daniel.cocu4@gmail.com>
2024-05-13 19:43:51 +0300

    update5_13_7.43

--- CatalogUnitBv/Form7.Designer.cs
+++ CatalogUnitBv/Form7.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form7";
+            button1 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            domainUpDown1 = new DomainUpDown();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(403, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(283, 296);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 1;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(204, 142);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(120, 23);
+            domainUpDown1.TabIndex = 2;
+            domainUpDown1.Text = "domainUpDown1";
+            // 
+            // Form7
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(domainUpDown1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button1);
+            Name = "Form7";
+            Text = "Form7";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private CheckedListBox checkedListBox1;
+        private DomainUpDown domainUpDown1;
     }
 }
