16658cd200a9671ca77aa2fc45caa222bf416c69
Daniel Cocu <daniel.cocu4@gmail.com>
2024-05-13 19:38:07 +0300

    update5_13_7.36

--- CatalogUnitBv/Form4.cs
+++ CatalogUnitBv/Form4.cs
@@ -15,17 +15,17 @@ namespace CatalogUnitBv
         public TI()
         {
             InitializeComponent();
-            TableLayoutPanel table = Note;
-            table.ColumnCount = 4;
-            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            table.Controls.Add(new Label() { Text = "Numele studentului", TextAlign = ContentAlignment.MiddleCenter }, 0, 0);
-            table.Controls.Add(new Label() { Text = "Materia", TextAlign = ContentAlignment.MiddleCenter }, 1, 0);
-            table.Controls.Add(new Label() { Text = "Nota", TextAlign = ContentAlignment.MiddleCenter }, 2, 0);
+            //TableLayoutPanel table = Note;
+            //table.ColumnCount = 4;
+            //table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            //table.Controls.Add(new Label() { Text = "Numele studentului", TextAlign = ContentAlignment.MiddleCenter }, 0, 0);
+            //table.Controls.Add(new Label() { Text = "Materia", TextAlign = ContentAlignment.MiddleCenter }, 1, 0);
+            //table.Controls.Add(new Label() { Text = "Nota", TextAlign = ContentAlignment.MiddleCenter }, 2, 0);
 
             // Setează dimensiunea celulelor pentru capul de tabel
-            table.ColumnStyles[0].SizeType = SizeType.AutoSize;
-            table.ColumnStyles[1].SizeType = SizeType.AutoSize;
-            table.ColumnStyles[2].SizeType = SizeType.AutoSize;
+            //table.ColumnStyles[0].SizeType = SizeType.AutoSize;
+            //table.ColumnStyles[1].SizeType = SizeType.AutoSize;
+            //table.ColumnStyles[2].SizeType = SizeType.AutoSize;
   
 
         }
--- CatalogUnitBv/Form5.Designer.cs
+++ CatalogUnitBv/Form5.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CatalogUnitBv
 {
-    partial class Form5
+    partial class NoteStudenti
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form5";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteStudenti));
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(28, 27, 31);
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(26, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(28, 29);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(75, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 21);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // NoteStudenti
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(28, 27, 31);
+            ClientSize = new Size(342, 513);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "NoteStudenti";
+            Text = "Note";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
     }
 }
